d4e1c742d3dc615a09a4d06581c18355971dddde
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-05-18 20:54:52 +0900

    feat: 메인 페이지의 추천 플레이스 기능 추가 (#19)
    추천 장소 기능 추가 확인

---
+++ src/js/components/MainContents/MainContentsView.js
@@ -0,0 +1,27 @@
+import { ViewComponent } from '../../core/ViewComponent.js';
+
+export class MainContentsView extends ViewComponent {
+  getTemplate(location) {
+    return `
+        <section class="main-place">
+            <div class="place-title">
+                <h3 class="place-title_head">주변 추천 플레이스</h3>
+                <div class="place-title_location">
+                    <img src="./src/asset/icon/location.svg" alt="" />
+                    <span>${location}</span>
+                </div>
+            </div>
+            <div class="place-recommend">
+                <div class="place-restaurant">
+                    <h5 class="place-recommend_title">음식점</h5>
+                </div>
+                <div class="place-cafe">
+                    <h5 class="place-recommend_title">카페</h5>
+                </div>
+                <div class="place-activity">
+                    <h5 class="place-recommend_title">액티비티</h5>
+                </div>
+            </div>
+        </section>`;
+  }
+}

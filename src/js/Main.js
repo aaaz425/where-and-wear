67fff4498f4c46bdcef6a9f92bdbd3295881ce78
qkdflrgs <qkdflrgs@gmail.com>
2023-05-18 20:51:12 +0900

    feat: Main.js에 추가된 기능 업데이트
    - Header 영역의 네비바 추가
    - 메인페이지의 추천 플레이스 추가
    - 이미지 슬라이더 기능 추가

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,19 @@
 import { $ } from './utils.js';
 import { App } from './app.js';
+import { HeaderView } from './components/Header/HeaderView.js';
+import { SliderModel } from './components/Slider/SliderModel.js';
+import { SliderView } from './components/Slider/SliderView.js';
+import { SliderController } from './components/Slider/SliderController.js';
+import { MainContentsView } from './components/MainContents/MainContentsView.js';
 
 class Main {
   constructor() {
     this.App = new App($('body'));
+    this.HeaderView = new HeaderView($('.header'));
+    this.MainContentsView = new MainContentsView($('.main'));
+    this.SliderModel = new SliderModel('dateSpot');
+    this.SliderView = new SliderView($('.place-recommend'));
+    this.SliderController = new SliderController(this.SliderModel, this.SliderView);
   }
 }
 

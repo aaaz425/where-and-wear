041d15d57dd6fcfefdfe8095f4a35bcda363ddaf
aaaz425 <aaaz425@naver.com>
2023-05-18 21:49:37 +0900

    feat: 프로젝트 머지 후 기능 조율
    프로젝트를 머지하고 분리된 기능을 서로 연결한다

---
+++ src/js/constant.js
@@ -0,0 +1,4 @@
+export const WEATHER = {
+  URL: 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?',
+  APIKEY: 'eMhtNJqH6t94wVWwqC3JSsCrXKLfv0H7vg65A%2FD3LKxOQn8c0nTX6jBmp8zBP9mEdkHKCDfA5qZfXd9OqVkzaw%3D%3D',
+};

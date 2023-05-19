fcd94409f396bbe580a64d0f857475b77ae4bd1a
qkdflrgs <qkdflrgs@gmail.com>
2023-05-19 14:56:42 +0900

    feat: Main.js 기능 추가

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,23 @@
 import { $ } from './utils.js';
-import { App } from './app.js';
+import { App } from './App.js';
+import { HeaderView } from './components/Header/HeaderView.js';
+import { SliderModel } from './components/Slider/SliderModel.js';
+import { SliderView } from './components/Slider/SliderView.js';
+import { SliderController } from './components/Slider/SliderController.js';
+import { MainContentsView } from './components/MainContents/MainContentsView.js';
+import { MainContentsModel } from './components/MainContents/MainContentsModel.js';
+import { MainContentsController } from './components/MainContents/MainContentsController.js';
 
 class Main {
   constructor() {
     this.App = new App($('body'));
+    this.HeaderView = new HeaderView($('.header'));
+    this.MainContentsView = new MainContentsView($('.main-place'));
+    this.MainContentsModel = new MainContentsModel();
+    this.MainContentsController = new MainContentsController(this.MainContentsModel, this.MainContentsView);
+    this.SliderModel = new SliderModel('dateSpot');
+    this.SliderView = new SliderView($('.main-place'));
+    this.SliderController = new SliderController(this.SliderModel, this.SliderView);
   }
 }
 

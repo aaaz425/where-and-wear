f3463281614f1be9049d6171394fe811053676ae
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-05-19 15:15:22 +0900

    [litae] feat/Slider: 이미지 슬라이더 기능 MVC 패턴 적용 (#33)
    * feat: 이미지 슬라이더 기능 추가
    
    이미지 슬라이더 기능(MVC 패턴 적용)
    
    * feat: Main.js에 추가된 기능 업데이트
    
    - Header 영역의 네비바 추가
    - 메인페이지의 추천 플레이스 추가
    - 이미지 슬라이더 기능 추가
    
    * feat: 이미지 슬라이더 기능 구현
    
    이미지 슬라이더(MVC 패턴) 구현
    
    ---------
    
    충돌해결완료
    Co-authored-by: TOKO <101464713+aaaz425@users.noreply.github.com>

--- src/js/components/Slider/SliderModel.js
+++ src/js/components/Slider/SliderModel.js
@@ -1,3 +1,5 @@
+import { getCurrentCoordinate } from '../../utils.js';
+
 export class SliderModel {
   constructor(url) {
     this.url = `http://localhost:3000/${url}`;
@@ -7,5 +9,24 @@ export class SliderModel {
     const response = await fetch(this.url);
     const data = await response.json();
     return data;
+  }
+
+  getCurrentAddress() {
+    return new Promise((resolve, reject) => {
+      const myLatLng = getCurrentCoordinate();
+      const geocoder = new google.maps.Geocoder();
+
+      geocoder.geocode({ location: myLatLng }, function (results, status) {
+        if (status === 'OK') {
+          if (results[0]) {
+            resolve(results[0].address_components[2].long_name);
+          } else {
+            reject('주소를 찾을 수 없습니다.');
+          }
+        } else {
+          reject('용산구');
+        }
+      });
+    });
   }
 }
--- src/js/components/Slider/SliderView.js
+++ src/js/components/Slider/SliderView.js
@@ -3,28 +3,79 @@ export class SliderView {
     this.target = target;
   }
 
-  getTemplate(lists) {
+  getTemplate(restaurant, cafe, activity) {
     return `
-      <div class="place-recommend_slider">
-        <button class="pre-btn"><img src="./src/asset/icon/chevron-left.svg" alt="" /></button>
-        <ul class="recommend-img_slider">
-          ${lists
-            .map(
-              list => `
-              <li class="place-list">
-                <img class="place-list_img" src="${list.img}" alt="" />
-                <span class="place-list_title">${list.name}</span>
-                <span class="place-list_info">${list.info}</span>
-                <span class="place-list_address">${list.address}</span>
-              </li>`
-            )
-            .join('')}
-        </ul>
-        <button class="next-btn"><img src="./src/asset/icon/chevron-right.svg" alt="" /></button>
-      </div>`;
+    <div class="place-recommend">
+      <div class="place-restaurant">
+        <h5 class="place-recommend_title">음식점</h5>
+        <div class="place-recommend_slider">
+          <button class="pre-btn"><img src="./src/asset/icon/chevron-left.svg" alt="" /></button>
+          <div class="slider-wrap">
+            <ul class="recommend-img_slider">
+              ${restaurant
+                .map(
+                  list => `
+                  <li class="place-list">
+                    <img class="place-list_img" src="${list.img}" alt="" />
+                    <span class="place-list_title">${list.name}</span>
+                    <span class="place-list_info">${list.info}</span>
+                    <span class="place-list_address">${list.address}</span>
+                  </li>`
+                )
+                .join('')}
+            </ul>
+          </div>
+          <button class="next-btn"><img src="./src/asset/icon/chevron-right.svg" alt="" /></button>
+        </div>
+      </div>
+      <div class="place-cafe">
+        <h5 class="place-recommend_title">카페</h5>
+        <div class="place-recommend_slider">
+          <button class="pre-btn"><img src="./src/asset/icon/chevron-left.svg" alt="" /></button>
+          <div class="slider-wrap">
+            <ul class="recommend-img_slider">
+              ${cafe
+                .map(
+                  list => `
+                  <li class="place-list">
+                    <img class="place-list_img" src="${list.img}" alt="" />
+                    <span class="place-list_title">${list.name}</span>
+                    <span class="place-list_info">${list.info}</span>
+                    <span class="place-list_address">${list.address}</span>
+                  </li>`
+                )
+                .join('')}
+            </ul>
+          </div>
+          <button class="next-btn"><img src="./src/asset/icon/chevron-right.svg" alt="" /></button>
+        </div>
+      </div>
+      <div class="place-activity">
+        <h5 class="place-recommend_title">액티비티</h5>
+        <div class="place-recommend_slider">
+          <button class="pre-btn"><img src="./src/asset/icon/chevron-left.svg" alt="" /></button>
+          <div class="slider-wrap">
+            <ul class="recommend-img_slider">
+              ${activity
+                .map(
+                  list => `
+                  <li class="place-list">
+                    <img class="place-list_img" src="${list.img}" alt="" />
+                    <span class="place-list_title">${list.name}</span>
+                    <span class="place-list_info">${list.info}</span>
+                    <span class="place-list_address">${list.address}</span>
+                  </li>`
+                )
+                .join('')}
+            </ul>
+          </div>
+          <button class="next-btn"><img src="./src/asset/icon/chevron-right.svg" alt="" /></button>
+        </div>
+      </div>
+    </div>`;
   }
 
-  render(data) {
-    this.target.insertAdjacentHTML('beforeend', this.getTemplate(data));
+  render(restaurant, cafe, activity) {
+    this.target.insertAdjacentHTML('beforeend', this.getTemplate(restaurant, cafe, activity));
   }
 }

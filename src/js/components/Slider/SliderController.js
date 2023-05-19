878f68d9eee2162c1222c6aa8928a78fcb5fd796
qkdflrgs <qkdflrgs@gmail.com>
2023-05-19 14:47:15 +0900

    feat: 이미지 슬라이더 기능 구현
    이미지 슬라이더(MVC 패턴) 구현

--- src/js/components/Slider/SliderController.js
+++ src/js/components/Slider/SliderController.js
@@ -1,3 +1,6 @@
+import { $ } from '../../utils.js';
+import { setCurrentPosition } from '../../utils.js';
+
 export class SliderController {
   constructor(model, view) {
     this.model = model;
@@ -6,6 +9,48 @@ export class SliderController {
   }
 
   async init() {
-    const data = await this.model.fetchData();
+    const address = await this.model.getCurrentAddress();
+    const location = await setCurrentPosition(address);
+    const fetchedData = await this.model.fetchData();
+
+    this.view.render(
+      fetchedData.district[location].restaurant,
+      fetchedData.district[location].cafe,
+      fetchedData.district[location].activity
+    );
+
+    this.setEvent();
+  }
+
+  setEvent() {
+    this.handleSlider('.place-restaurant');
+    this.handleSlider('.place-cafe');
+    this.handleSlider('.place-activity');
+  }
+
+  handleSlider(sliderClass) {
+    const SLIDE_MARGIN = 42;
+    const sliderElement = document.querySelector(sliderClass);
+    const sliderContainer = sliderElement.querySelector('.recommend-img_slider');
+    const preBtn = sliderElement.querySelector('.pre-btn');
+    const nextBtn = sliderElement.querySelector('.next-btn');
+    const sliderItems = sliderElement.querySelectorAll('.place-list');
+    const itemWidth = sliderItems[0].offsetWidth;
+    const stepSize = itemWidth + SLIDE_MARGIN;
+
+    let currentPosition = 0;
+
+    preBtn.addEventListener('click', () => {
+      if (currentPosition >= 0) return;
+      currentPosition += stepSize;
+      sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+    });
+
+    nextBtn.addEventListener('click', () => {
+      if (currentPosition < -stepSize) return;
+      currentPosition -= stepSize;
+      sliderContainer.style.transform = `translateX(${currentPosition}px)`;
+      sliderContainer.style.transition = `all 500ms ease`;
+    });
   }
 }

48c5d8a63a820767607bdb87eca4b9e26664c8f7
TOKO <101464713+aaaz425@users.noreply.github.com>
2023-05-19 14:20:27 +0900

    feat: 오늘의 의상 추천 mvc 패턴 적용 (#30)
    오늘의 의상 추천 기능에 mvc 패턴을 적용시킨다
    
    확인완료

--- src/js/components/Outfit/OutfitInfoView.js
+++ src/js/components/Outfit/OutfitInfoView.js
@@ -1,62 +1,39 @@
-import { OutfitZoneView } from './OutfitZoneView.js';
-import { $, addFocusClass, removeFocusClass } from '../../utils.js';
-
-export class OutfitInfoView extends OutfitZoneView {
-  constructor(target, model, step) {
-    super(target);
-    this.model = model;
+export class OutfitInfoView {
+  constructor(target, gender, step, index) {
+    this.target = target;
+    this.gender = gender;
     this.step = step;
-    this.index = [0, 0, 1, 0, 1];
+    this.index = index;
 
-    this.setEvent();
+    this.render();
   }
 
-  getInfoTemplate(data, step, index) {
+  getTemplate(gender, step, index) {
     return `
       <div class="jacket">
         <span class="outfit-category">자켓</span>
-        <span class="outfit-item">${data[step].jacket[index[0]].detail}</span>
+        <span class="outfit-item">${gender[step].jacket[index[0]].detail}</span>
       </div>
       <div class="top">
         <span class="outfit-category">상의</span>
-        <span class="outfit-item">${data[step].top[index[1]].detail}</span>
+        <span class="outfit-item">${gender[step].top[index[1]].detail}</span>
       </div>
       <div class="bottom">
         <span class="outfit-category">하의</span>
-        <span class="outfit-item">${data[step].bottom[index[2]].detail}</span>
+        <span class="outfit-item">${gender[step].bottom[index[2]].detail}</span>
       </div>
       <div class="shoes">
         <span class="outfit-category">신발</span>
-        <span class="outfit-item">${data[step].shoes[index[3]].detail}</span>
+        <span class="outfit-item">${gender[step].shoes[index[3]].detail}</span>
       </div>
       <div class="accessory">
         <span class="outfit-category">액세서리</span>
-        <span class="outfit-item">${data[step].accessory[index[4]].detail}</span>
+        <span class="outfit-item">${gender[step].accessory[index[4]].detail}</span>
       </div>
     `;
   }
 
-  async renderInfo(model) {
-    this.target.insertAdjacentHTML('beforeend', await this.getInfoTemplate(model, this.step, this.index));
-  }
-
-  setEvent() {
-    this.target.parentNode.addEventListener('click', async e => {
-      if (e.target.classList.contains('male')) {
-        addFocusClass($('.male'));
-        removeFocusClass($('.female'));
-        this.getZoneTemplate(await this.model.getMaleOutfit(), this.step, this.index);
-        this.getInfoTemplate(await this.model.getMaleOutfit(), this.step, this.index);
-        this.renderZone(this.model);
-        this.renderInfo(this.model);
-      } else if (e.target.classList.contains('female')) {
-        addFocusClass($('.female'));
-        removeFocusClass($('.male'));
-        this.getZoneTemplate(await this.model.getFemaleOutfit(), this.step, this.index);
-        this.getInfoTemplate(await this.model.getFemaleOutfit(), this.step, this.index);
-        this.renderZone(this.model);
-        this.renderInfo(this.model);
-      }
-    });
+  async render() {
+    this.target.insertAdjacentHTML('beforeend', this.getTemplate(await this.gender, await this.step, this.index));
   }
 }
--- src/js/components/Outfit/OutfitZoneView.js
+++ src/js/components/Outfit/OutfitZoneView.js
@@ -1,24 +1,24 @@
-import { ViewComponent } from '../../core/ViewComponent.js';
-
-export class OutfitZoneView extends ViewComponent {
-  constructor(target, model, step) {
-    super(target);
-    this.model = model;
+export class OutfitZoneView {
+  constructor(target, gender, step, index) {
+    this.target = target;
+    this.gender = gender;
     this.step = step;
-    this.index = [0, 0, 1, 0, 1];
+    this.index = index;
+
+    this.render();
   }
 
-  getZoneTemplate(data, step, index) {
+  getTemplate(gender, step, index) {
     return `
-      <img class="jacket" src="${data[step].jacket[index[0]].img}" />
-      <img class="top" src="${data[step].top[index[1]].img}" />
-      <img class="bottom" src="${data[step].bottom[index[2]].img}" />
-      <img class="shoes" src="${data[step].shoes[index[3]].img}" />
-      <img class="accessory" src="${data[step].accessory[index[4]].img}" />
+      <img class="jacket" src="${gender[step].jacket[index[0]].img}" />
+      <img class="top" src="${gender[step].top[index[1]].img}" />
+      <img class="bottom" src="${gender[step].bottom[index[2]].img}" />
+      <img class="shoes" src="${gender[step].shoes[index[3]].img}" />
+      <img class="accessory" src="${gender[step].accessory[index[4]].img}" />
     `;
   }
 
-  async renderZone(model) {
-    this.target.insertAdjacentHTML('beforeend', await this.getZoneTemplate(model, this.step, this.index));
+  async render() {
+    this.target.insertAdjacentHTML('beforeend', this.getTemplate(await this.gender, await this.step, this.index));
   }
 }

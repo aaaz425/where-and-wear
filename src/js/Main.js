348b8f6750f093c4d67458a970d73ec56e1e55b6
litae <109706689+qkdflrgs@users.noreply.github.com>
2023-05-19 15:08:49 +0900

    [litae] feat/Main-place: 메인페이지의 추천 플레이스 영역 MVC 패턴 적용 (#32)
    * feat: 메인 페이지의 추천 플레이스 기능 추가
    
    * refactor: 메인 영역의 추천 플레이스 MVC 패턴 적용
    
    * style: 추천 플레이스 CSS 수정
    
    * style: HTML 구글맵 API 스크립트 추가
    
    구글맵 API 스크립트 추가
    
    * feat: App.js 메인 영역 추가
    
    * feat: Main.js 기능 추가
    
    * feat: utils 업데이트
    
    - setCurrentPosition(지역명을 한글에서 영문으로 변경하는 함수) 추가
    - getCurrentCoordinate(사용자의 현재 위치 좌표를 구하는 함수) 추가
    
    ---------
    
    충돌해결완료
    Co-authored-by: TOKO <101464713+aaaz425@users.noreply.github.com>

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,10 +1,12 @@
 import { $ } from './utils.js';
-import { App } from './app.js';
+import { App } from './App.js';
 import { HeaderView } from './components/Header/HeaderView.js';
 import { SliderModel } from './components/Slider/SliderModel.js';
 import { SliderView } from './components/Slider/SliderView.js';
 import { SliderController } from './components/Slider/SliderController.js';
 import { MainContentsView } from './components/MainContents/MainContentsView.js';
+import { MainContentsModel } from './components/MainContents/MainContentsModel.js';
+import { MainContentsController } from './components/MainContents/MainContentsController.js';
 import { ModelComponent } from './core/ModelComponent.js';
 import { OutfitMainView } from './components/Outfit/OutfitMainView.js';
 import { OutfitZoneView } from './components/Outfit/OutfitZoneView.js';
@@ -15,13 +17,17 @@ import { WeatherImgView } from './components/Weather/WeatherImgView.js';
 import { WeatherDetailView } from './components/Weather/WeatherDetailView.js';
 import { WeatherModel } from './components/Weather/WeatherModel.js';
 
+
 class Main {
   constructor() {
     this.App = new App($('body'));
     this.HeaderView = new HeaderView($('.header'));
-    this.MainContentsView = new MainContentsView($('.main'));
+
+    this.MainContentsView = new MainContentsView($('.main-place'));
+    this.MainContentsModel = new MainContentsModel();
+    this.MainContentsController = new MainContentsController(this.MainContentsModel, this.MainContentsView);
     this.SliderModel = new SliderModel('dateSpot');
-    this.SliderView = new SliderView($('.place-recommend'));
+    this.SliderView = new SliderView($('.main-place'));
     this.SliderController = new SliderController(this.SliderModel, this.SliderView);
 
     this.WeatherModel = new WeatherModel();
--- src/js/utils.js
+++ src/js/utils.js
@@ -4,6 +4,51 @@ export function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
+export function setCurrentPosition(location) {
+  const positions = {
+    용산구: 'yongsan',
+    강남구: 'gangnam',
+    강서구: 'gangseo',
+    관악구: 'gwanak',
+    금천구: 'geumcheon',
+    구로구: 'guro',
+    노원구: 'nowon',
+    동작구: 'dongjak',
+    도봉구: 'dobong',
+    동대문구: 'dongdaemun',
+    송파구: 'songpa',
+    성동구: 'seongdong',
+    서대문구: 'seodaemun',
+    서초구: 'seocho',
+    성북구: 'seongbuk',
+    강동구: 'gangdong',
+    강북구: 'gangbuk',
+    영등포구: 'yeongdeungpo',
+    양천구: 'yangcheon',
+    은평구: 'eunpyeong',
+    마포구: 'mapo',
+    중구: 'jung',
+    중랑구: 'jungnang',
+  };
+
+  if (location in positions) {
+    return positions[location];
+  } else {
+    return 'yongsan';
+  }
+}
+
+export function getCurrentCoordinate() {
+  const defaultCoordinate = { lat: 37.5323264, lng: 126.9907031 };
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(position => {
+      return { lat: position.coords.latitude, lng: position.coords.longitude };
+    });
+  }
+
+  return defaultCoordinate;
+}
+
 export const addFocusClass = node => {
   node.classList.add('focus');
 };
